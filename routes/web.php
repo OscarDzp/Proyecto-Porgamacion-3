e63b0504531d76d6afce183b2e3d09f18068b8c5
crodrigueza8 <801080860@castrocarazo.ac.cr>
2023-04-04 10:47:01 -0600

    rutas editadas

--- routes/web.php
+++ routes/web.php
@@ -16,21 +16,21 @@ use App\Http\Controllers\ReaccionController;
 |
 */
 
-//Route::get('/',[UsuarioController::class, 'index'])->name('usuarios.index');
-//Route::get('/crear',[UsuarioController::class, 'create'])->name('usuarios.create');
-//Route::post('/',[UsuarioController::class, 'store'])->name('usuarios.store');
-//Route::get('/{usuario}',[UsuarioController::class, 'show'])->name('usuarios.show');
-//Route::get('/{usuario}/editar',[UsuarioController::class, 'edit'])->name('usuarios.edit');
-//Route::patch('/{usuario}',[UsuarioController::class, 'update'])->name('usuarios.update');
-//Route::delete('/{usuario}',[UsuarioController::class, 'destroy'])->name('usuarios.destroy');
-
-//Route::get('/',[PublicacionController::class, 'index'])->name('publicaciones.index');
-//Route::get('/crear',[PublicacionController::class, 'create'])->name('publicaciones.create');
-//Route::post('/',[PublicacionController::class, 'store'])->name('publicaciones.store');
-//Route::get('/{publicacion}',[PublicacionController::class, 'show'])->name('publicaciones.show');
-//Route::get('/{publicacion}/editar',[PublicacionController::class, 'edit'])->name('publicaciones.edit');
-//Route::patch('/{publicacion}',[PublicacionController::class, 'update'])->name('publicaciones.update');
-//Route::delete('/{publicacion}',[PublicacionController::class, 'destroy'])->name('publicaciones.destroy');
+//Route::get('/usuarios',[UsuarioController::class, 'index'])->name('usuarios.index');
+//Route::get('/crear/usuarios',[UsuarioController::class, 'create'])->name('usuarios.create');
+//Route::post('/usuarios',[UsuarioController::class, 'store'])->name('usuarios.store');
+//Route::get('/usuarios/{usuario}',[UsuarioController::class, 'show'])->name('usuarios.show');
+//Route::get('/usuarios/{usuario}/editar',[UsuarioController::class, 'edit'])->name('usuarios.edit');
+//Route::patch('/usuarios/{usuario}/update',[UsuarioController::class, 'update'])->name('usuarios.update');
+//Route::delete('/usuarios/{usuario}/destroy',[UsuarioController::class, 'destroy'])->name('usuarios.destroy');
+//
+//Route::get('/publicaciones',[PublicacionController::class, 'index'])->name('publicaciones.index');
+//Route::get('/publicaciones/crear',[PublicacionController::class, 'create'])->name('publicaciones.create');
+//Route::post('/publicaciones',[PublicacionController::class, 'store'])->name('publicaciones.store');
+//Route::get('/publicaciones/{publicacion}',[PublicacionController::class, 'show'])->name('publicaciones.show');
+//Route::get('/publicaciones/{publicacion}/editar',[PublicacionController::class, 'edit'])->name('publicaciones.edit');
+//Route::patch('/publicaciones/{publicacion}/update',[PublicacionController::class, 'update'])->name('publicaciones.update');
+//Route::delete('/publicaciones/{publicacion}/destroy',[PublicacionController::class, 'destroy'])->name('publicaciones.destroy');
 
 //Route::get('/',[ReaccionController::class, 'index'])->name('reacciones.index');
 //Route::get('/crear',[ReaccionController::class, 'create'])->name('reacciones.create');
@@ -40,8 +40,8 @@ use App\Http\Controllers\ReaccionController;
 //Route::patch('/{reaccion}',[ReaccionController::class, 'update'])->name('reacciones.update');
 //Route::delete('/{reaccion}',[ReaccionController::class, 'destroy'])->name('reacciones.destroy');
 
-//Route:: resource("usuarios", UsuarioController::class);
-//Route:: resource("publicaciones", PublicacionController::class);
-//Route:: resource("reacciones", ReaccionController::class);
-//
+Route:: resource("/usuarios", UsuarioController::class);
+Route:: resource("/publicaciones", PublicacionController::class);
+Route:: resource("/reacciones", ReaccionController::class);
+
 
